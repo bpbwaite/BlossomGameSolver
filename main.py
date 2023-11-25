07423f66ba702f36410e250985991804b13f0ce7
Briellyn <bpbraithwaite@gmail.com>
2023-11-24 21:32:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Merriam Webster Blossom Game Solver
-# Briellyn B - 20 Nov 2023
+# Briellyn B - 24 Nov 2023
 
-import time
 from urllib.request import urlretrieve
+import requests
+import re
+from datetime import date
+import time
 
 def timing_decorator(func):
     def wrapper(*args, **kwargs):
@@ -45,7 +48,7 @@ def peakBonusLetter(word: str, centerLetter: str):
     return ''.join(sorted(list(set(most[0]))))
 
 @timing_decorator
-def getSolutions(dictionary: str, petals: str, center: str, bonus: str, display: int = 20):
+def getSolutions(dictionary: str, petals: str, center: str, bonus: str, display: int = 20) -> bool:
 
     petals = (petals + center[0] + bonus[0]).replace(' ', '').lower()
 
@@ -75,7 +78,7 @@ def getSolutions(dictionary: str, petals: str, center: str, bonus: str, display:
         display = len(valid_solutions)
     if display == 0:
         print('No Solutions')
-        exit()
+        return False
 
     print(f'-- Showing {display}/{len(valid_solutions)} solutions:')
     for word in valid_solutions[0:min(display, len(valid_solutions))]:
@@ -86,27 +89,72 @@ def getSolutions(dictionary: str, petals: str, center: str, bonus: str, display:
         if isPanagram(word, petals):
             print('(Panagram)', end = '')
         print('')
-    return
+    return True
+
+def copyWeeklyDictToFile(fname:str):
+    root = 'https://www.merriam-webster.com'
+    game = '/games/blossom-word-game'
+    page = requests.get(root + game).text
+    link1 = re.compile(r'src="(/dist-cross.+?compiled.+?\.js)"', flags=re.I|re.M|re.X).search(page).group(1)
+    distribution_root = link1.split('/js/')[0] + '/js/'
+    page = requests.get(root + link1).text
+    link2_version = re.compile(r'(vendors-node_modules_mw-semipublic_blossom_dist_blossom_js)[": ]{0,3}"([a-z0-9]+)"', flags=re.I|re.M|re.X).search(page)
+    link2 = distribution_root + link2_version.group(1) + "." + link2_version.group(2) + ".js"
+    page = requests.get(root + link2).text
+    auth_code = re.compile(r'Authorization:.*"Bearer.*"\.concat\("(.*?)"\)', flags=re.I|re.M|re.X).search(page).group(1)
+
+    headers =   {
+                    "Authorization": "Bearer " + auth_code,
+                    "Content-type": "application/json"
+                }
+    #payload = r'{"query":"\nquery {\nblossom (\nsort: [\"published_date\"]\nfilter: {\npublished_date: {\n_gte: \"' \
+    #    + str(date.today()).replace('-', '/').strip() \
+    #    + r'\"\n}\n}\n) {\npuzzle_id\npublished_date\ncenter\nletters\npangram \nwords\n}\n}"}'
+    payload = r'{"query":"\nquery {\nblossom (\nsort: [\"published_date\"]\n'\
+        + r'\n) {\npuzzle_id\npublished_date\ncenter\nletters\npangram \nwords\n}\n}"}'
+
+    x = requests.post(url='https://prod-mw-cms.m-w.com/graphql', headers=headers, data=payload)
+
+    with open(fname, 'wt+', encoding='utf-8') as OutFile:
+        for puzzle in x.json()['data']['blossom']:
+            for word in puzzle['words']:
+                OutFile.write(word.strip().lower() + '\n')
+
 
 def main():
-    dictionary = 'words_alpha.txt'
-    try:
-        open(dictionary, mode='r')
-    except FileNotFoundError:
-        uri = 'https://github.com/dwyl/english-words/raw/master/words_alpha.txt'
-        print('No dictionary found. Downloading...')
-        urlretrieve(uri, dictionary)
 
-    print('Dictionary downloaded!')
-
-    disp = int(input('Display Limit? '))
+    #disp = int(input('Display Limit? '))
+    disp = 24
     petal_letters = (input('Outer letters? '))
     center_letter = (input('Center letter? '))[0]
+
     while True:
         bonus_letter = input('Bonus letter? ')[0]
         print('Searching...')
-
-        getSolutions(dictionary, petal_letters, center_letter, bonus_letter, display=disp)
+        dictionary = 'weekly.txt'
+        try:
+            open(dictionary, mode='r')
+        except FileNotFoundError:
+            print('Getting weekly database...')
+            copyWeeklyDictToFile(dictionary)
+            print('Done')
+        solveSuccess = getSolutions(dictionary, petal_letters, center_letter, bonus_letter, display=disp)
+        if not solveSuccess:
+            print('Retrying weekly database...')
+            copyWeeklyDictToFile(dictionary)
+            print('Done')
+            solveSuccess = getSolutions(dictionary, petal_letters, center_letter, bonus_letter, display=disp)
+            if not solveSuccess:
+                print('Falling back to built-in dictionary')
+                dictionary = 'words_alpha.txt'
+                try:
+                    open(dictionary, mode='r')
+                except FileNotFoundError:
+                    print('No built-in dictionary found. Downloading...')
+                    uri = 'https://github.com/dwyl/english-words/raw/master/words_alpha.txt'
+                    urlretrieve(uri, dictionary)
+                print('Dictionary downloaded')
+                solveSuccess = getSolutions(dictionary, petal_letters, center_letter, bonus_letter, display=disp)
 
 if __name__ == '__main__':
     main()
